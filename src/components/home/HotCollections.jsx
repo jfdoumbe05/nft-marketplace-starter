186f838ee99e6e6b3afbda17a78858ae4687703a
Jean Doumbe <jf.doumbe@gmail.com>
2026-09-20 06:40:39 -0500

    add hot collections carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -2,9 +2,20 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
   const [hotCollections, setHotCollections] = useState([]);
+  const settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+};
 
   useEffect(() => {
     fetch("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
@@ -13,17 +24,20 @@ const HotCollections = () => {
   }, []);
 
   return (
-    <section id="section-collections" className="no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
-          </div>
-          {hotCollections.slice(0, 4).map((collection) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={collection.id}>
+   <div className="container">
+  <div className="row">
+    <div className="col-lg-12">
+      <div className="text-center">
+        <h2>Hot Collections</h2>
+        <div className="small-border bg-color-2"></div>
+      </div>
+    </div>
+  </div>
+
+  <Slider {...settings}>
+    {hotCollections.map((collection) => ( 
+ 
+            <div key={collection.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
@@ -45,9 +59,8 @@ const HotCollections = () => {
               </div>
             </div>
           ))}
-        </div>
-      </div>
-    </section>
+          </Slider>
+        </div> 
   );
 };
 
