6bf556c7d4b9258c6b4a5ed951094dcfd9f66f0c
Jean Doumbe <jf.doumbe@gmail.com>
2026-09-22 23:55:42 -0500

    fetch and display dynamic author data

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
 
 const Author = () => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+  fetch(
+    "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=73855012"
+  )
+    .then((res) => res.json())
+  .then((data) => {
+  setAuthor(data);
+});
+}, []);
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +36,17 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={author?.authorImage || AuthorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {author?.authorName || "Monica Lucas"}
+                          <span className="profile_username">
+  @{author?.tag || "monicaaaa"}
+</span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {author?.address || "UDHUHWud..."}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,7 +57,9 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
+                      <div className="profile_follower">
+  {author?.followers ?? 0} followers
+</div>
                       <Link to="#" className="btn-main">
                         Follow
                       </Link>
@@ -55,7 +70,10 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                <AuthorItems
+  nftCollection={author?.nftCollection || []}
+  authorImage={author?.authorImage}
+/>
                 </div>
               </div>
             </div>
